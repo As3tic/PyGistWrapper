8c7353d257dfb261aafbe5168dc41d51e10ed0fb
Abhinav KM <abhinav.m@zerone-consulting.com>
2023-02-06 14:48:30 +0530

    Restructured code

--- gistblog/main.py
+++ gistblog/main.py
@@ -3,11 +3,15 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+from gistwrapper import Gist
+
+from dotenv import load_dotenv
+load_dotenv()
 
 class Token(BaseModel):
     gist_token: str
 
-gist_token = ""
+client = Gist()
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -16,12 +20,24 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def request_token(request: Request):
-    return templates.TemplateResponse("token-accept.html", {"request": request})
+    if client.gist_token == None:
+        return templates.TemplateResponse("token-accept.html", {"request": request})
+    return {
+        "message":"Welcome"
+    }
 
 
 @app.post("/set-token")
 def set_token(token: Token):
-    global gist_token # replace with session shenanigans and verify the token!
-    gist_token = token.gist_token
+    client.set_token(token=token.gist_token)
+
     return 200
 
+
+@app.get("/me")
+def who_am_i(request: Request):
+    identiy = client.get.Gist(category="")[0].author
+
+    data = identiy.__dict__
+
+    return templates.TemplateResponse("user-page.html", {"request": request, "user":data})
